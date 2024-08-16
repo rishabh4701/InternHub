633100699e45c5e77ed21fb4bfb2fe0ff617753f
kanishqdhangar <131457610+kanishqdhangar@users.noreply.github.com>
2024-08-16 12:36:03 +0530

    Update serializer.py

--- backend/mnit/internship_portal/internship/serializer.py
+++ backend/mnit/internship_portal/internship/serializer.py
@@ -5,3 +5,13 @@ class InternshipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Internship
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+        # Override the default update method to customize how updates are handled
+        instance.Title = validated_data.get('Title', instance.Title)
+        instance.Mentor = validated_data.get('Mentor', instance.Mentor)
+        instance.Duration = validated_data.get('Duration', instance.Duration)
+        instance.Status = validated_data.get('Status', instance.Status)
+        instance.Skills = validated_data.get('Skills', instance.Skills)
+        instance.save()
+        return instance
