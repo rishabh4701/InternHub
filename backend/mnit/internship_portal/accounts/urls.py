6517116c2c9a6d8c291478dcac3d3fe359322835
kanishqdhangar <131457610+kanishqdhangar@users.noreply.github.com>
2024-08-16 12:33:35 +0530

    Update urls.py

--- backend/mnit/internship_portal/accounts/urls.py
+++ backend/mnit/internship_portal/accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, UserListView, SendEmailView, VerifyOTPView
 
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserListView.as_view(), name='user-detail'),
+    path('send-email/', SendEmailView.as_view(), name='send_email'),
+    path('verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
     # path('signup/', views.postData),
 ]
